80cb5ee809cb745bac59400dc889828e0b3452b8
hwalborn <hwalborn@gmail.com>
2021-04-25 22:39:06 -0400

    removed unused code

--- async-await/chainedDrive.js
+++
@@ -1,6 +0,0 @@
-const directions = require('../env/directions');
-const utilities = require('../utilities');
-
-directions.reduce((promiseChain, direction) => {
-    return promiseChain.then(_ => utilities.driveAction(direction[0], direction[1]));
-}, Promise.resolve());
